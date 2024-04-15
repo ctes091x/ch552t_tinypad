16312a35a83881a8d6f21f26164f6842b46332fb
ctes091x <98365960+ctes091x@users.noreply.github.com>
2024-04-15 18:47:24 +0900

    feat: Create ch552t_tinypad_sketch.ino

--- ch552t_tinypad_sketch/ch552t_tinypad_sketch.ino
+++ ch552t_tinypad_sketch/ch552t_tinypad_sketch.ino
@@ -18,21 +18,30 @@
 
 #include "./src/userUsbHidKeyboard/USBHIDKeyboard.h"
 
-#define BUTTON1_PIN 11
-#define BUTTON2_PIN 31
-#define BUTTON3_PIN 30
+#define BUTTON1_PIN 33
+#define BUTTON2_PIN 34
+#define BUTTON3_PIN 35
+#define BUTTON4_PIN 30
+#define BUTTON5_PIN 31
+#define BUTTON6_PIN 11
 
-#define LED_BUILTIN 33
+#define LED_BUILTIN 10
 
 bool button1PressPrev = false;
 bool button2PressPrev = false;
 bool button3PressPrev = false;
+bool button4PressPrev = false;
+bool button5PressPrev = false;
+bool button6PressPrev = false;
 
 void setup() {
   USBInit();
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
   pinMode(BUTTON3_PIN, INPUT_PULLUP);
+  pinMode(BUTTON4_PIN, INPUT_PULLUP);
+  pinMode(BUTTON5_PIN, INPUT_PULLUP);
+  pinMode(BUTTON6_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -62,18 +71,57 @@ void loop() {
   if (button2PressPrev != button2Press) {
     button2PressPrev = button2Press;
     if (button2Press) {
-      // Keyboard_press(KEY_LEFT_GUI);
-      // Keyboard_press('r');
-      // Keyboard_releaseAll();
-      // delay(100);
-      // Keyboard_write('c');
-      // Keyboard_write('o');
-      // Keyboard_write('d');
-      // Keyboard_write('e');
-      // Keyboard_write(0xE0);
-      Keyboard_press(KEY_LEFT_ALT);
-      Keyboard_write(KEY_TAB);
-      Keyboard_release(KEY_LEFT_ALT);
+      Keyboard_press(KEY_LEFT_GUI);
+      Keyboard_press('r');
+      Keyboard_releaseAll();
+      delay(100);
+      Keyboard_write('c');
+      Keyboard_write('o');
+      Keyboard_write('d');
+      Keyboard_write('e');
+      Keyboard_write(0xE0);
+      // Keyboard_press(KEY_LEFT_ALT);
+      // Keyboard_write(KEY_TAB);
+      // Keyboard_release(KEY_LEFT_ALT);
+    }
+  }
+
+  // bool button3Press = !digitalRead(BUTTON3_PIN);
+  // if (button3PressPrev != button3Press) {
+  //   button3PressPrev = button3Press;
+  //   if (button3Press) {
+  //     Keyboard_press('a');
+  //   }
+  // }
+  digitalWrite(LED_BUILTIN, !digitalRead(BUTTON3_PIN));
+
+  bool button4Press = !digitalRead(BUTTON4_PIN);
+  if (button4PressPrev != button4Press) {
+    button4PressPrev = button4Press;
+    if (button4Press) {
+      Keyboard_press(KEY_LEFT_CTRL);
+      Keyboard_press(KEY_LEFT_GUI);
+      Keyboard_press(KEY_LEFT_ARROW);
+      Keyboard_releaseAll();
+    }
+  }
+  bool button5Press = !digitalRead(BUTTON5_PIN);
+  if (button5PressPrev != button5Press) {
+    button5PressPrev = button5Press;
+    if (button5Press) {
+      Keyboard_press(KEY_LEFT_GUI);
+      Keyboard_press(KEY_TAB);
+      Keyboard_releaseAll();
+    }
+  }
+  bool button6Press = !digitalRead(BUTTON6_PIN);
+  if (button6PressPrev != button6Press) {
+    button6PressPrev = button6Press;
+    if (button6Press) {
+      Keyboard_press(KEY_LEFT_CTRL);
+      Keyboard_press(KEY_LEFT_GUI);
+      Keyboard_press(KEY_RIGHT_ARROW);
+      Keyboard_releaseAll();
     }
   }
 
